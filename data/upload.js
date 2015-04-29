b8884a1fe0ec7f2773a0d34e34d4fd21369f28ec
Aaron Lidman <aaronlidman@gmail.com>
2015-04-29 18:34:35 -0400

    Upload script

---
+++ data/upload.js
@@ -0,0 +1,23 @@
+var upload = require('mapbox-upload');
+
+// node upload <file> <account> <accesstoken> <mapid>
+
+progress = upload({
+    file: process.argv[2],
+    account: process.argv[3],
+    accesstoken: process.argv[4], // A valid Mapbox API secret token with the map:write scope enabled.
+    mapid: process.argv[5]
+});
+
+progress.on('error', function(err){   
+    if (err) throw err;
+});
+
+progress.on('progress', function(p){
+    console.log(p.percentage);
+});
+
+progress.once('finished', function(){   
+    // Upload has completed but is likely queued for processing and not yet available on Mapbox.
+    console.log('upload successful and processing on mapbox.com');
+});
