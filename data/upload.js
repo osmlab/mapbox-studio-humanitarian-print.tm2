c348bed509fd80c4d1a73e591f7802e80ff42547
Aaron Lidman <aaronlidman@gmail.com>
2015-05-06 12:10:24 -0400

    Arg

--- data/upload.js
+++ data/upload.js
@@ -1,13 +1,13 @@
 var upload = require('mapbox-upload');
-var arg = process.argv;
+var argv = process.argv;
 
 if (!argv[5]) return console.log('usage: node upload.js <file> <account> <accesstoken> <mapid>');
 
 progress = upload({
-    file: process.argv[2],
-    account: process.argv[3],
-    accesstoken: process.argv[4], // A valid Mapbox API secret token with the map:write scope enabled.
-    mapid: process.argv[5]
+    file: argv[2],
+    account: argv[3],
+    accesstoken: argv[4], // A valid Mapbox API secret token with the map:write scope enabled.
+    mapid: argv[5]
 });
 
 progress.on('error', function(err){   
